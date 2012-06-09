1dfd3117d48ce7080a38a4c1862c156657a58608
Spencer Sugarman <spencersugarman@gmail.com>
2012-06-09 08:22:14 -0400

    Refactor js into a Sugarman class

--- js/site.js
+++ js/site.js
@@ -1,79 +1,93 @@
-$("#s").hover(
-	function(){
-		$(this).attr('src', "assets/logo/s.gif");
-	},
-	function(){
-		$(this).attr('src', "assets/logo/ss.png");
-	}
-);
+var Sugarman = function () {
+	var root = this;
+	
+	root.projects = {};
 
-var texts = {};
+	root.init = function () {
+		// load projects into root.projects
+		$('#projects .piece').each(function(){
+			var project = $(this).attr('data-id');
+			root.load(project);
+		});
+	
+		// if example.com/#projectName then show the project
+		if (window.location.hash.substring(1) !== '') {
+			var project = window.location.hash.substring(1);
+			root.show(project);
+		}
 
-if (window.location.hash.substring(1) !== '') {
-	var project = window.location.hash.substring(1);
-	load(project, function(){
-		$('#intro').hide();
-		$('#content .container').html(texts[project]).parent().show();
-	});
-}
-
-$('#projects .piece').each(function(){
-	var project = $(this).attr('data-id');
-	load(project);
-});
-
-function load (project, callbackFunc) {
-	if (texts[project] === undefined) {
-		$.get('texts/'+project+'.html', function (response) {
-			texts[project] = response;
-			if (callbackFunc) callbackFunc();
+		// event listener for animating the logo
+		$("#s").hover(
+			function(){
+				$(this).attr('src', "assets/logo/s.gif");
+			},
+			function(){
+				$(this).attr('src', "assets/logo/ss.png");
+			}
+		);
+		
+		// event listener for hashchange
+		$(window).hashchange(function () {
+			if (window.location.hash.substring(1) !== '') {
+				var project = window.location.hash.substring(1);
+				root.show(project);
+			} else {
+				$('#content').slideUp(600, function () {
+					setTimeout(function(){
+						$('#intro').slideDown();
+						root.scrollToTop(400);
+					}, 200);
+				});
+			}
+		});
+		
+		// event listener for clicking a project link
+		$('#projects').on('click', '.piece', function () {
+			var project = $(this).attr('data-id');
+			if (window.location.hash === '#'+project) root.scrollToTop();
+			else window.location.hash = project;
 		});
 	}
-}
-
-function show (project) {
-	var text;
-	if (texts[project] === undefined) {
-		$.get('texts'/+a+'.html', function (response) {
-			text = texts[project] = response;
-		});
-	} else {
-		text = texts[project];
+	
+	root.load = function (project, callbackFunc) {
+		if (root.projects[project] === undefined) {
+			$.get('texts/'+project+'.html', function (response) {
+				root.projects[project] = response;
+				if (callbackFunc) callbackFunc();
+			});
+		}
 	}
-	if ($('#intro').is(':visible')) slideUp('#intro', 400);
-	else slideUp('#content', 600);
-	function slideUp (el, speed) {
-		scrollToTop(speed);
-		$(el).slideUp(speed, function(){
-			setTimeout(function(){
-				$('#content .container').html(text);
-				$('#content').slideDown(800);
-			}, 200);
-		});
-	}				
-}
-
-function scrollToTop (speed) {
-	speed = (speed) ? speed : 200;
-	$('html, body').animate({scrollTop:0}, speed);
-}
-
-$(window).hashchange(function () {
-	if (window.location.hash.substring(1) !== '') {
-		var project = window.location.hash.substring(1);
-		show(project);
-	} else {
-		$('#content').slideUp(600, function () {
-			setTimeout(function(){
-				$('#intro').slideDown();
-				scrollToTop(400);
-			}, 200);
-		});
+	
+	root.show = function (project) {
+		// if the project text has not loaded
+		if (root.projects[project] === undefined) {
+			// run load() with show() as callback
+			root.load(project, root.show);
+			return;
+		} else {
+			var text = root.projects[project];
+		}
+		
+		// show the project
+		if ($('#intro').is(':visible')) slideUp('#intro', 400);
+		else slideUp('#content', 600);
+		function slideUp (el, speed) {
+			root.scrollToTop(speed);
+			$(el).slideUp(speed, function(){
+				setTimeout(function(){
+					$('#content .container').html(text);
+					$('#content').slideDown(800);
+				}, 200);
+			});
+		}				
 	}
-});
+	
+	root.scrollToTop = function (speed) {
+		speed = (speed) ? speed : 200;
+		$('html, body').animate({scrollTop:0}, speed);
+	}
 
-$('#projects').on('click', '.piece', function () {
-	var project = $(this).attr('data-id');
-	if (window.location.hash === '#'+project) scrollToTop();
-	else window.location.hash = project;
-});
+};
+
+var spencer = new Sugarman();
+spencer.init();
